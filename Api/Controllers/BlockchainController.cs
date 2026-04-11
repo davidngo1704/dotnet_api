2a030197c828a4017cb5671de50453d887917ce3
dai <davidngo1704@gmail.com>
2026-04-11 14:22:23 +0700

    sa

--- Api/Controllers/BlockchainController.cs
+++ Api/Controllers/BlockchainController.cs
@@ -17,43 +17,44 @@ namespace Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetBalance()
         {
+            string endpoint = "http://192.168.1.9:1704/linux/execute";
 
-            var mexcResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var mexcResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/mexc && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var kucoinResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var kucoinResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/kucoin && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var bingxResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var bingxResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/bingx && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var okxResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var okxResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/okx && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var binanceResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var binanceResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/binance && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var bybitResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var bybitResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/bybit && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var bitgetResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var bitgetResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/bitget && source .venv/bin/activate && python balance_spot.py"
             });
 
-            var gateResponse = _httpService.PostAsync<CommonRequest, CommonResponse>("http://192.168.1.9:1704/linux/execute", new CommonRequest()
+            var gateResponse = _httpService.PostAsync<CommonRequest, CommonResponse>(endpoint, new CommonRequest()
             {
                 command = "cd /var/lib/ApiGateway/cold_wallet/blockchain/cex/gate && source .venv/bin/activate && python balance_spot.py"
             });
