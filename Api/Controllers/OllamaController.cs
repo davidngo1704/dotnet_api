6df8ceb5b06386c4237d8722ad57e1684613d5cf
dai <davidngo1704@gmail.com>
2026-04-18 23:08:21 +0700

    s

--- Api/Controllers/OllamaController.cs
+++ Api/Controllers/OllamaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api.Database;
+using Api.Database.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -7,9 +9,25 @@ namespace Api.Controllers
     [ApiController]
     public class OllamaController : ControllerBase
     {
+        private readonly AppDbContext _db;
+        public OllamaController(AppDbContext db)
+        {
+            _db = db;
+        }
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+            var data = _db.Users.ToList();
+            return Ok(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            _db.Users.Add(user);
+
+            await _db.SaveChangesAsync();
+
+
             return Ok("OllamaController is working!");
         }
     }
--- Api/README.md
+++ Api/README.md
@@ -1,1 +1,5 @@
 sudo ufw allow 5000/tcp
+
+dotnet ef migrations add Init
+
+dotnet ef database update
