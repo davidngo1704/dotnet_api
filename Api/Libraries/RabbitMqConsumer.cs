247bb8c36e83a4f6d011382ce868bbc00c82d688
dai <davidngo1704@gmail.com>
2026-04-19 14:29:40 +0700

    a

---
+++ Api/Libraries/RabbitMqConsumer.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Api.Libraries;
+
+public class RabbitMqConsumer : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+
+        var connection = await factory.CreateConnectionAsync();
+        var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(
+            queue: "demo-queue",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (sender, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"[x] Received: {message}");
+
+            await Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(
+            queue: "demo-queue",
+            autoAck: true,
+            consumer: consumer
+        );
+    }
+}
---
+++ Api/Libraries/RabbitMqProducer.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Api.Libraries;
+
+public class RabbitMqProducer
+{
+    private IConnection? _connection;
+    private IChannel? _channel;
+
+    public async Task RabbitMqProducerInit()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "192.168.1.15"
+        };
+
+        _connection = await factory.CreateConnectionAsync();
+        _channel = await _connection.CreateChannelAsync();
+
+        await _channel.QueueDeclareAsync(
+            queue: "demo-queue",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+    }
+
+    public async Task SendMessage(string message)
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+
+        await _channel.BasicPublishAsync(
+            exchange: "",
+            routingKey: "demo-queue",
+            body: body
+        );
+
+    }
+
+}
