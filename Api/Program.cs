f96740b034255020b78ed0bca04da94b52a7da1d
dai <davidngo1704@gmail.com>
2026-04-19 14:31:09 +0700

    sa

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHostedService<RabbitMqConsumer>();
+
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 builder.Services.AddControllers();
