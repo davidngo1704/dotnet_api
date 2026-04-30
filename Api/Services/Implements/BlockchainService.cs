eb7ec04ae2726ec1f368cdf11b1e789a9e7fcfc5
dai <davidngo1704@gmail.com>
2026-05-01 03:37:58 +0700

    a

--- Api/Services/Implements/BlockchainService.cs
+++ Api/Services/Implements/BlockchainService.cs
@@ -22,6 +22,18 @@ namespace Api.Services.Implements
             };
 
             FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/binance/price.json", System.Text.Json.JsonSerializer.Serialize(result));
+
+            var client = new BingXClient(
+                "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
+                "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
+            );
+
+            var resultBingx = await client.GetPositions();
+
+            FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/bingx/result.json", System.Text.Json.JsonSerializer.Serialize(resultBingx));
+
+
+
         }
 
 
