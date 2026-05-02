bc82929b24997d93ee930953b1e57779ea4320ba
dai <davidngo1704@gmail.com>
2026-05-02 10:39:32 +0700

    sa

--- Api/Services/Implements/BlockchainService.cs
+++ Api/Services/Implements/BlockchainService.cs
@@ -13,36 +13,44 @@ namespace Api.Services.Implements
         }
         public async Task TriggerMinute()
         {
-            var data = await _httpService.GetAsync<CoinPriceModel>("https://api.binance.com/api/v3/ticker/price?symbol=WLDUSDT");
+            var dataBTC = await _httpService.GetAsync<CoinPriceModel>("https://api.binance.com/api/v3/ticker/price?symbol=WLDUSDT");
 
-            var result = new CoinPriceResponse()
+            var resultBTC = new CoinPriceResponse()
             {
-                data = data,
+                data = dataBTC,
                 datetime = DateTime.UtcNow.AddHours(7).ToString("yyyy-MM-dd HH:mm:ss")
             };
 
-            FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/binance/price.json", System.Text.Json.JsonSerializer.Serialize(result));
+            FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/binance/priceBTC.json", System.Text.Json.JsonSerializer.Serialize(resultBTC));
 
-            var clientBingx = new BingXClient(
-                "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
-                "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
-            );
+            //var clientBingx = new BingXClient(
+            //    "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
+            //    "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
+            //);
 
-            var resultBingx = await clientBingx.GetPositions();
+            //var resultBingx = await clientBingx.GetPositions();
 
-            FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/bingx/result.json", resultBingx);
+            //FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/bingx/result.json", resultBingx);
 
-            var clientBinance = new BinanceClient(
-                "06MzlJ1aV3quq7f8WnBPp73iHLpNEkFgGBLTVFmjEJ0W29bLXIVNJ7WUgG64LnYb",
-                "rrof6lQsbXMTyeuSVkBbS1WbQWUnJbB7gotZgOND2TyFTmdnAtI5MdEUGUjWZzZI"
-            );
+            //var clientBinance = new BinanceClient(
+            //    "06MzlJ1aV3quq7f8WnBPp73iHLpNEkFgGBLTVFmjEJ0W29bLXIVNJ7WUgG64LnYb",
+            //    "rrof6lQsbXMTyeuSVkBbS1WbQWUnJbB7gotZgOND2TyFTmdnAtI5MdEUGUjWZzZI"
+            //);
 
-            var resultBinance = await clientBinance.GetSpotBalance();
+            //var resultBinance = await clientBinance.GetSpotBalance();
 
-            FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/binance/result.json", resultBinance);
+            //FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/binance/result.json", resultBinance);
 
 
+            var dataETH = await _httpService.GetAsync<CoinPriceModel>("https://api.binance.com/api/v3/ticker/price?symbol=WLDUSDT");
 
+            var resultETH = new CoinPriceResponse()
+            {
+                data = dataETH,
+                datetime = DateTime.UtcNow.AddHours(7).ToString("yyyy-MM-dd HH:mm:ss")
+            };
+
+            FileHelper.WriteText("/var/lib/ApiGateway/blockchain/cex/binance/priceETH.json", System.Text.Json.JsonSerializer.Serialize(resultETH));
 
         }
 
