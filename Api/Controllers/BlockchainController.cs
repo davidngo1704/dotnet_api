de9dab08543861274b0de1ebbac2527eecb8c3b1
Ngô Thành Đại <davidngo1704@gmail.com>
2026-05-12 13:19:28 +0700

    sa

--- Api/Controllers/BlockchainController.cs
+++ Api/Controllers/BlockchainController.cs
@@ -44,6 +44,82 @@ namespace Api.Controllers
             await _blockchainService.TriggerHour();
             return Ok(DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss"));
         }
+        [HttpGet]
+        public async Task<IActionResult> BingxGetPosition()
+        {
+            var client = new BingXClient(
+                "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
+                "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
+            );
 
+            var resultString = await client.GetPositions();
+
+            var result = JsonConvert.DeserializeObject<BingxApiResponse>(resultString);
+
+            var finalResult = new List<object>();
+
+            foreach (var item in result?.Data!)
+            {
+                finalResult.Add(new
+                {
+                    Symbol = item.Symbol,
+                    TaiXiu = item.PositionSide == "LONG" ? "LONG" : "SHORT",
+                    LaiLo = item.UnrealizedProfit,
+                    GiaThanhLy = item.LiquidationPrice,
+                    GiaDanhDau = item.MarkPrice,
+                    GiaVaoLenh = item.AvgPrice,
+                    DonBay = item.Leverage,
+                });
+            }
+
+            return Ok(finalResult);
+        }
+        //[HttpGet]
+        //public async Task<IActionResult> BinanceGetPosition()
+        //{
+        //    var client = new BinanceClient(
+        //        "06MzlJ1aV3quq7f8WnBPp73iHLpNEkFgGBLTVFmjEJ0W29bLXIVNJ7WUgG64LnYb",
+        //        "rrof6lQsbXMTyeuSVkBbS1WbQWUnJbB7gotZgOND2TyFTmdnAtI5MdEUGUjWZzZI"
+        //    );
+
+        //    var result = await client.GetSpotBalance();
+
+        //    var data = JsonConvert.DeserializeObject<AccountInfo>(result);
+
+        //    var resultReal = new List<Balance>();
+
+        //    if (data?.Balances != null)
+        //    {
+        //        foreach (var item in data.Balances)
+        //        {
+        //            if (item.Free > 0)
+        //            {
+        //                if (item.Asset != null && item.Asset.StartsWith("LD"))
+        //                {
+        //                    item.Asset = item.Asset.Replace("LD", "");
+        //                }
+
+        //                if (item.Asset == "USDT")
+        //                {
+        //                    item.Price = 1;
+        //                    resultReal.Add(item);
+        //                    continue;
+        //                }
+
+        //                var dataPrice = await _httpService.GetAsync<CoinPriceModel>(@$"https://api.binance.com/api/v3/ticker/price?symbol={item.Asset}USDT");
+
+        //                item.Price = dataPrice?.price ?? 0;
+
+        //                resultReal.Add(item);
+        //            }
+        //        }
+        //    }
+
+        //    return Ok(new
+        //    {
+        //        Total = resultReal.Sum(x => x.Free * x.Price),
+        //        Coin = resultReal,
+        //    });
+        //}
     }
 }
