3844b0af53be9dd69d5a4787b7eb09133cbd5c14
dai <davidngo1704@gmail.com>
2026-05-21 10:53:16 +0700

    s

--- Api/Controllers/BlockchainController.cs
+++ Api/Controllers/BlockchainController.cs
@@ -73,6 +73,102 @@ namespace Api.Controllers
 
             return Ok(finalResult);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> BingxClosePosition()
+        {
+            var client = new BingXClient(
+                "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
+                "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
+            );
+
+            var resultString = await client.GetPositions();
+
+            var result = JsonConvert.DeserializeObject<BingxApiResponse>(resultString);
+
+            var finalResult = new List<object>();
+
+            foreach (var item in result?.Data!)
+            {
+                finalResult.Add(new
+                {
+                    Symbol = item.Symbol,
+                    TaiXiu = item.PositionSide == "LONG" ? "LONG" : "SHORT",
+                    LaiLo = item.UnrealizedProfit,
+                    GiaThanhLy = item.LiquidationPrice,
+                    GiaDanhDau = item.MarkPrice,
+                    GiaVaoLenh = item.AvgPrice,
+                    DonBay = item.Leverage,
+                });
+            }
+
+            return Ok(finalResult);
+        }
+        [HttpGet]
+        public async Task<IActionResult> BingxShort()
+        {
+            var client = new BingXClient(
+                "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
+                "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
+            );
+
+            var resultString = await client.GetPositions();
+
+            var result = JsonConvert.DeserializeObject<BingxApiResponse>(resultString);
+
+            var finalResult = new List<object>();
+
+            foreach (var item in result?.Data!)
+            {
+                finalResult.Add(new
+                {
+                    Symbol = item.Symbol,
+                    TaiXiu = item.PositionSide == "LONG" ? "LONG" : "SHORT",
+                    LaiLo = item.UnrealizedProfit,
+                    GiaThanhLy = item.LiquidationPrice,
+                    GiaDanhDau = item.MarkPrice,
+                    GiaVaoLenh = item.AvgPrice,
+                    DonBay = item.Leverage,
+                });
+            }
+
+            return Ok(finalResult);
+        }
+        [HttpGet]
+        public async Task<IActionResult> BingxLong()
+        {
+            var client = new BingXClient(
+                "tl77B8Cu3kD6qOO98qofzJb6M5dbTMl7KTL1ddUHREHCjvXxRp5ARHWq5j9uMGlguNyBNLIHaBYq16kQ",
+                "x0XW5WkWjWLxwHV4S2AQ9JlC64rvwgI7IIe0bdzh3qeXqR4lpRd2BakBwTeEIEG0QTPPmI5TfHDg6CtV6DQ"
+            );
+
+            var resultString = await client.GetPositions();
+
+            var result = JsonConvert.DeserializeObject<BingxApiResponse>(resultString);
+
+            var finalResult = new List<object>();
+
+            foreach (var item in result?.Data!)
+            {
+                finalResult.Add(new
+                {
+                    Symbol = item.Symbol,
+                    TaiXiu = item.PositionSide == "LONG" ? "LONG" : "SHORT",
+                    LaiLo = item.UnrealizedProfit,
+                    GiaThanhLy = item.LiquidationPrice,
+                    GiaDanhDau = item.MarkPrice,
+                    GiaVaoLenh = item.AvgPrice,
+                    DonBay = item.Leverage,
+                });
+            }
+
+            return Ok(finalResult);
+        }
+
+
+
+
+
         //[HttpGet]
         //public async Task<IActionResult> BinanceGetPosition()
         //{
